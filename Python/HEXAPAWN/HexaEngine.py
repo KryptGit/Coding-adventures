90a419797a6e23303176097f24e99fce51566c55
siddhartha227 <70448046+siddhartha227@users.noreply.github.com>
2022-01-16 22:34:08 -0600

    Update HexaEngine.py

--- Python/HEXAPAWN/HexaEngine.py
+++ Python/HEXAPAWN/HexaEngine.py
@@ -9,9 +9,35 @@ class Board:
 class functions:
     def __init__(self):
       pass
-    def win_check(board):
-        if board[7] == "w" or board[7] == "w" or board[7] == "w":
+    def win_check(board, white_moves,black_moves):
+        if board[7] == "w" or board[8] == "w" or board[9] == "w":
             return 1
+        elif board[1] == "b" or board[2] == "b" or board[3] == "b":
+            return -1
+        elif "w" not in board:
+            return -1
+        elif "b" not in board:
+            return 1
+        elif white_moves == []:
+            return -1
+        elif black_moves == []:
+            return 1
+        else: 
+            return 0
+      def edit(action ,pos, board, player = None):
+    default_value = str(pos)
+    if action == "remove":
+      board[pos] = default_value
+    if action == "add":
+      board[pos] = player
+  def move(player, startingpos, targetpostion, board):
+    if not startingpos == player:
+      print('you cant move the opponents peice')
+      return 
+    edit("remove", startingpos, board)
+    edit('add', targetpostion, board, player)
+
+
 
 ip = int(input("h"))
 col = (ip-1)%3
