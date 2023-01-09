3da3ab41b5775799b73cb26c61ab253860a3a032
siddhartha227 <70448046+siddhartha227@users.noreply.github.com>
2023-01-09 14:13:54 -0600

    initialized proj tut

---
+++ Js/tut/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>My first vue app</h1>
+  <Modal></Modal>
+</template>
+
+<script>
+import Modal from './components/modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Js/tut/src/components/modal.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <p>modal contant</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.modal
+{
+
+    width: 400px;
+    background-color: white;
+    border-radius: 10px;
+    padding: 20px;
+    margin: 100px auto;
+
+
+}
+
+.backdrop
+{
+    top: 0;
+    position: fixed;
+    background-color: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+
+
+</style>
